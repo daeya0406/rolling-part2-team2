1caa884d600e7fb07cd7450a755b7680cc48e04e
narae <narae@naraeui-MacBookPro.local>
2025-09-29 12:40:12 +0900

    feat: 버튼 컴포넌트 및 아이콘 컴포넌트 추가

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Icon from "./Icon";
+import "./Button.scss";
+
+function Button({
+  label,
+  size = "lg",
+  variant = "primary",
+  icon,
+  disabled = false,
+  className = "",
+  onClick,
+  ...props
+}) {
+  return (
+    <button
+      className={`btn btn--${size} btn--${variant} ${
+        !label && icon && className !== "btn--icon-box"
+          ? `btn--icon btn--icon-${size}` // 여기 수정됨
+          : ""
+      } ${className}`}
+      disabled={disabled}
+      onClick={onClick}
+      {...props}
+    >
+      {icon && label && (
+        // 아이콘 + 텍스트일 때
+        <span className="btn__icon btn__icon--left">
+          <Icon name={icon} />
+        </span>
+      )}
+
+      {icon && !label && (
+        // 아이콘만 있을 때
+        <span className="btn__icon">
+          <Icon name={icon} />
+        </span>
+      )}
+
+      {label && <span className="btn__label">{label}</span>}
+    </button>
+  );
+}
+
+export default Button;
