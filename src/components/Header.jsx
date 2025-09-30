c7887e33affe9ab0733137b051af123297f60ff8
narae <narae@naraeui-MacBookPro.local>
2025-09-30 13:16:58 +0900

    헤더 컴포넌트 및 테스트 페이지에 헤더영역  추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
 import { Link } from "react-router-dom";
 import "./header.scss";
+import Button from "./ui/Button";
+import Icon from "./ui/Icon";
 
-export default function Header() {
+function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/components1">components1</Link>
-        <Link to="/components2">components2</Link>
-        <Link to="/typo">typo</Link>
-        <Link to="/color">color</Link>
-      </nav>
+    <header className="header">
+      <div className="header__inner">
+        <div className="header__logo">
+          <Link to="/">
+            <Icon name="logo" className="logo-icon" />
+          </Link>
+        </div>
+
+        <nav className="header__nav">
+          <Link to="/">Home</Link>
+          <Link to="/components1">components1</Link>
+          <Link to="/components2">components2</Link>
+          <Link to="/typo">typo</Link>
+          <Link to="/color">color</Link>
+        </nav>
+
+        <div className="header__btn">
+          <Link to="/post">
+            <Button label="롤링 페이퍼 만들기" size="md" variant="outline" />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
+
+export default Header;
--- src/pages/Test/TestComponents1/index.jsx
+++ src/pages/Test/TestComponents1/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../../../components/ui/Button";
+import Header from "../../../components/Header";
 
 function Components1() {
   return (
