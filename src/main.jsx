e497123edd982f18aa6ad49c298aecd2fba9f4ee
daeya0406 <daeya0406@gmail.com>
2025-10-14 22:23:23 +0900

    feat : vercel/analytics 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/routes";
 import "./assets/scss/global.scss";
 import "pretendard/dist/web/static/pretendard.css";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
